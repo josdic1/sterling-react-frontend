b958878f0d64ef5df9fc57a21442ea332e9b87b3
josdic1 <joshuadicker@gmail.com>
2026-02-01 15:32:49 -0500

    ok

--- src/pages/ReservationDetailPage.jsx
+++ src/pages/ReservationDetailPage.jsx
@@ -7,7 +7,6 @@ import { AttendeeForm } from "../components/attendees/AttendeeForm";
 import { SaveFloater } from "../components/shared/SaveFloater";
 import { Edit3, UserPlus, Clock, DollarSign } from "lucide-react";
 
-console.log("Build version: 1.0.1 - Checking HTTPS");
 const API_URL = "https://sterling-fastapi-backend-production.up.railway.app";
 
 export function ReservationDetailPage() {
